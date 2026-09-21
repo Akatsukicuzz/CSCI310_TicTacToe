4be3bf58a3382e810da1e3085bdf2c537b8f5b89
Esther060109 <agilitydog96@gmail.com>
2026-09-21 18:47:56 -0500

    add Players.c and main.cpp for player function implementations and game execution

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include "Players.cpp"
+
+int main() 
+{ 
+
+    // test();
+    Player1();
+    display();
+    // Call the each player function in a separate thread.      
+    //when the game is over, join the threads and exit the program.
+    return 0;
+};
